6b9f6e48ff463285bdc27fee49b27469015e0df4
Elymas-Magus <41921067+Elymas-Magus@users.noreply.github.com>
2021-07-03 00:24:49 -0300

    Create 2990.sql

---
+++ 2990.sql
@@ -0,0 +1,1 @@
+SELECT empregados.cpf, empregados.enome, departamentos.dnome FROM empregados INNER JOIN departamentos ON empregados.dnumero = departamentos.dnumero WHERE empregados.cpf NOT IN (SELECT cpf_emp FROM trabalha) ORDER BY empregados.cpf
